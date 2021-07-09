a85923e082f5bb50b6a185b909ee96ea257813ea
_ilahi <adi.shrivastava2905@gmail.com>
2021-07-10 02:44:28 +0530

    search problem solved

--- src/components/users/Repos.tsx
+++ src/components/users/Repos.tsx
@@ -3,48 +3,93 @@ import GithubContext from '../../contexts/github/GithubContext';
 import RepoItem from './RepoItem';
 
 export interface Props {
-   searchText?: string
+	searchText?: string;
 }
 
 const Repos: React.FC<Props> = ({ searchText }) => {
+	const { repos } = useContext(GithubContext);
 
-   const { repos, getUserRepos } = useContext(GithubContext);
+	const [searchedRepos, setSearchRepos] = useState(repos);
 
-   const [searchedRepos, setSearchRepos] = useState(repos)
+	const levenshteinDistance = (str1 = '', str2 = '') => {
+		const track = Array(str2.length + 1)
+			.fill(null)
+			.map(() => Array(str1.length + 1).fill(null));
+		for (let i = 0; i <= str1.length; i += 1) {
+			track[0][i] = i;
+		}
+		for (let j = 0; j <= str2.length; j += 1) {
+			track[j][0] = j;
+		}
+		for (let j = 1; j <= str2.length; j += 1) {
+			for (let i = 1; i <= str1.length; i += 1) {
+				const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+				track[j][i] = Math.min(
+					track[j][i - 1] + 1, // deletion
+					track[j - 1][i] + 1, // insertion
+					track[j - 1][i - 1] + indicator // substitution
+				);
+			}
+		}
+		// console.log(
+		// 	str1,
+		// 	str2,
+		// 	track[str2.length][str1.length],
+		// 	str1.length - str2.length - 1
+		// );
+		return track[str2.length][str1.length] < str1.length - str2.length + 2;
+	};
 
-   // // if repo is empty
-   // const handleNoRepos = async () => {
-   //    await getUserRepos('ujjwalnahar');
-   // }
+	// // if repo is empty
+	// const handleNoRepos = async () => {
+	//    await getUserRepos('ujjwalnahar');
+	// }
 
-   // seaarching by full_name and language
-   const handleSearch = () => {
-      const searchedRepos = repos.filter((repo: any) => ((repo.full_name.toLowerCase().search(searchText?.toLowerCase()) > -1) || (repo?.language?.toLowerCase().search(searchText?.toLowerCase()) > -1)))
+	// seaarching by full_name and language
+	const handleSearch = () => {
+		const searchedRepos = repos.filter(
+			(repo: any) =>
+				repo.full_name.toLowerCase().search(searchText?.toLowerCase()) >
+					-1 ||
+				repo?.language?.toLowerCase().search(searchText?.toLowerCase()) > -1
+		);
 
-      setSearchRepos([...searchedRepos])
-   }
+		setSearchRepos([...searchedRepos]);
+	};
 
-   useEffect(() => {
-      if (repos.length === 0) {
-         // handleNoRepos();
-      }
-      console.log(repos)
-      setSearchRepos([...repos])
+	useEffect(() => {
+		if (repos.length === 0) {
+			// handleNoRepos();
+		}
 
+		if (searchText) {
+			let filteredSearch = repos.filter((repo: any) =>
+				levenshteinDistance(
+					repo.name.toLowerCase(),
+					searchText.toLowerCase()
+				)
+			);
 
-      if (repos.length === 0 && searchText) {
-         handleSearch();
-      }
+			console.log(filteredSearch);
 
-   }, [repos, searchText])
+			setSearchRepos([...filteredSearch]);
+		} else {
+			setSearchRepos([...repos]);
+		}
 
+		if (repos.length === 0 && searchText) {
+			handleSearch();
+		}
+	}, [repos, searchText]);
 
-   return (
-      <div className="container p-5 repo-grid">
-         {searchedRepos && searchedRepos.map((repo: any, index: number) => <RepoItem key={repo.id} index={index} repo={repo} />)}
-      </div>
+	return (
+		<div className="container p-5 repo-grid">
+			{searchedRepos &&
+				searchedRepos.map((repo: any, index: number) => (
+					<RepoItem key={repo.id} index={index} repo={repo} />
+				))}
+		</div>
+	);
+};
 
-   );
-}
-
-export default Repos
+export default Repos;
--- src/components/users/user-page/RightContainer.tsx
+++ src/components/users/user-page/RightContainer.tsx
@@ -1,54 +1,71 @@
 import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import GithubContext from '../../../contexts/github/GithubContext';
 import Repos from '../Repos';
 
-interface Props {
-
-}
+interface Props {}
 
 const RightContainer: React.FC<Props> = () => {
+	const { user, repos } = useContext(GithubContext);
+	const { following, followers, hireable } = user;
 
-   const { user, repos } = useContext(GithubContext);
-   const { following, followers, hireable } = user;
+	const [repoSearch, setRepoSearch] = useState<string>('');
 
-   console.log(repos, user)
+	return (
+		<div className="col-lg-8 right-container">
+			<div className="row top-grid">
+				<div className="col-3">
+					<p className="top-grid-head">Followers</p>
+					<p className="top-grid-text">{followers} 🐱‍🏍</p>
+				</div>
+				<div className="col-3">
+					<p className="top-grid-head">Following</p>
+					<p className="top-grid-text">{following} 🚀 </p>
+				</div>
+				<div className="col-3">
+					<p className="top-grid-head">Total repos</p>
+					<p className="top-grid-text">{repos.length} 😎</p>
+				</div>
+				<div className="col-3">
+					<p className="top-grid-head">Hireable</p>
+					<p className="top-grid-text">
+						{hireable ? (
+							<FontAwesomeIcon
+								style={{ color: 'green' }}
+								icon={faCheck}
+							/>
+						) : (
+							<FontAwesomeIcon
+								style={{ color: 'rgb(159, 18, 60)' }}
+								icon={faTimes}
+							/>
+						)}
+					</p>
+				</div>
+			</div>
 
-   const [repoSearch, setRepoSearch] = useState<string>('')
+			<h1 className="repo-line text-center">Repositories 🚀</h1>
+			<form
+				className="repo-form"
+				onSubmit={(evt: any) => {
+					evt.preventDefault();
+					console.log(`repo search ${repoSearch}`);
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Search by Repository or language"
+					className="repo-input"
+					value={repoSearch}
+					onChange={(evt) => setRepoSearch(evt.target.value)}
+				/>
+				{/* <button type="submit" className="btn btn-submit form-control repo-sbt"><FontAwesomeIcon icon={faSearch} style={{ color: 'black' }} /></button> */}
+			</form>
 
-   return (
-      <div className="col-lg-8 right-container">
-         <div className="row top-grid">
-            <div className="col-3">
-               <p className="top-grid-head">Followers</p>
-               <p className="top-grid-text">{followers} 🐱‍🏍</p>
-            </div>
-            <div className="col-3">
-               <p className="top-grid-head">Following</p>
-               <p className="top-grid-text">{following} 🚀 </p>
-            </div>
-            <div className="col-3">
-               <p className="top-grid-head">Total repos</p>
-               <p className="top-grid-text">{repos.length} 😎</p></div>
-            <div className="col-3">
-               <p className="top-grid-head">Hireable</p>
-               <p className="top-grid-text">{hireable ? (<FontAwesomeIcon style={{ color: 'green' }} icon={faCheck} />) : (<FontAwesomeIcon style={{ color: 'rgb(159, 18, 60)' }} icon={faTimes} />)}</p></div>
-         </div>
+			<Repos searchText={repoSearch} />
+		</div>
+	);
+};
 
-
-         <h1 className="repo-line text-center">
-            Repositories 🚀
-         </h1>
-         <form className="repo-form" onSubmit={(evt: any) => { evt.preventDefault(); console.log(`repo search ${repoSearch}`); }}>
-            <input type="text" placeholder="Search by Repository or language" className="repo-input" value={repoSearch} onChange={(evt) => setRepoSearch(evt.target.value)} />
-            {/* <button type="submit" className="btn btn-submit form-control repo-sbt"><FontAwesomeIcon icon={faSearch} style={{ color: 'black' }} /></button> */}
-         </form>
-
-         <Repos searchText={repoSearch} />
-
-      </div>
-   )
-}
-
-export default RightContainer
+export default RightContainer;
